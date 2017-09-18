b3a5ad0b3cbb6dea3b66203103777d8a48a3ce6e
liyangbin <liyanbin98@gmail.com>
2017-09-18 21:12:39 +0800

    create groups index

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,52 @@
+class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @goup.save
+      flash[:notice] = "Created Success!"
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+      if @group.update(group_params)
+        flash[:notice] = "Update Success!"
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path
+  end
+
+  Private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
+
+
+end
